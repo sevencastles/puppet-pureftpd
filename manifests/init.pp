046f79ab0974bb8889f6c2e6a9f6ad65898fea3f
Carlo Rengo <info@carlorengo.it>
2019-04-02 12:19:53 +0200

    Initial commit to remove deprecate messages.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,54 +1,43 @@
 # Class: pureftpd:  See README.md for documentation.
 class pureftpd (
-  $config             = {},
-  $config_db_dir      = $pureftpd::params::config_db_dir,
-  $config_dir         = $pureftpd::params::config_dir,
-  $config_manage      = $pureftpd::params::config_manage,
-  $dir_aliases        = {},
-  $dir_aliases_file   = $pureftpd::params::dir_aliases_file,
-  $install_options    = undef,
-  $ldap_config        = {},
-  $ldap_config_file   = $pureftpd::params::ldap_config_file,
-  $manage_ssl         = $pureftpd::params::manage_ssl,
-  $mysql_config       = {},
-  $mysql_config_file  = $pureftpd::params::mysql_config_file,
-  $package_ensure     = $pureftpd::params::package_ensure,
-  $package_manage     = $pureftpd::params::package_manage,
-  $package_name       = $pureftpd::params::package_name,
-  $package_name_ldap  = $pureftpd::params::package_name_ldap,
-  $package_name_mysql = $pureftpd::params::package_name_mysql,
-  $package_name_pgsql = $pureftpd::params::package_name_pgsql,
-  $pgsql_config       = {},
-  $pgsql_config_file  = $pureftpd::params::pgsql_config_file,
-  $purge_conf_dir     = $pureftpd::params::purge_conf_dir,
-  $restart            = $pureftpd::params::restart,
-  $server_mode        = $pureftpd::params::server_mode,
-  $server_type        = $pureftpd::params::server_type,
-  $service_enabled    = $pureftpd::params::service_enabled,
-  $service_manage     = $pureftpd::params::service_manage,
-  $service_name       = $pureftpd::params::service_name,
-  $service_name_ldap  = $pureftpd::params::service_name_ldap,
-  $service_name_mysql = $pureftpd::params::service_name_mysql,
-  $service_name_pgsql = $pureftpd::params::service_name_pgsql,
-  $service_provider   = $pureftpd::params::service_provider,
-  $ssl_config         = {},
-  $ssl_pemfile        = $pureftpd::params::ssl_pemfile,
-  $uploadgid          = $pureftpd::params::uploadgid,
-  $uploaduid          = $pureftpd::params::uploaduid,
-  $uploadscript       = $pureftpd::params::uploadscript,
-  $virtualchroot      = $pureftpd::params::virtualchroot
+  Hash                        $config             = {},
+  String                      $config_db_dir      = $pureftpd::params::config_db_dir,
+  String                      $config_dir         = $pureftpd::params::config_dir,
+  Boolean                     $config_manage      = $pureftpd::params::config_manage,
+  Hash                        $dir_aliases        = {},
+  String                      $dir_aliases_file   = $pureftpd::params::dir_aliases_file,
+                              $install_options    = undef,
+  Hash                        $ldap_config        = {},
+  String                      $ldap_config_file   = $pureftpd::params::ldap_config_file,
+  Boolean                     $manage_ssl         = $pureftpd::params::manage_ssl,
+  Hash                        $mysql_config       = {},
+  String                      $mysql_config_file  = $pureftpd::params::mysql_config_file,
+  String                      $package_ensure     = $pureftpd::params::package_ensure,
+  Boolean                     $package_manage     = $pureftpd::params::package_manage,
+  String                      $package_name       = $pureftpd::params::package_name,
+  String                      $package_name_ldap  = $pureftpd::params::package_name_ldap,
+  String                      $package_name_mysql = $pureftpd::params::package_name_mysql,
+  String                      $package_name_pgsql = $pureftpd::params::package_name_pgsql,
+  Hash                        $pgsql_config       = {},
+  String                      $pgsql_config_file  = $pureftpd::params::pgsql_config_file,
+  Boolean                     $purge_conf_dir     = $pureftpd::params::purge_conf_dir,
+  Boolean                     $restart            = $pureftpd::params::restart,
+  String                      $server_mode        = $pureftpd::params::server_mode,
+  String                      $server_type        = $pureftpd::params::server_type,
+  Boolean                     $service_enabled    = $pureftpd::params::service_enabled,
+  Boolean                     $service_manage     = $pureftpd::params::service_manage,
+  String                      $service_name       = $pureftpd::params::service_name,
+  String                      $service_name_ldap  = $pureftpd::params::service_name_ldap,
+  String                      $service_name_mysql = $pureftpd::params::service_name_mysql,
+  String                      $service_name_pgsql = $pureftpd::params::service_name_pgsql,
+  String                      $service_provider   = $pureftpd::params::service_provider,
+  Hash                        $ssl_config         = {},
+  String                      $ssl_pemfile        = $pureftpd::params::ssl_pemfile,
+  Variant[String, Integer] $uploadgid          = $pureftpd::params::uploadgid,
+  Variant[String, Integer] $uploaduid          = $pureftpd::params::uploaduid,
+  String                      $uploadscript       = $pureftpd::params::uploadscript,
+  Boolean                     $virtualchroot      = $pureftpd::params::virtualchroot
 ) inherits pureftpd::params {
-
-  validate_bool($config_manage, $manage_ssl, $package_manage, $restart)
-  validate_bool($purge_conf_dir, $service_enabled, $service_manage)
-  validate_hash($config, $dir_aliases,$ldap_config, $mysql_config)
-  validate_hash($pgsql_config, $ssl_config)
-  validate_string($config_db_dir, $config_dir, $dir_aliases_file)
-  validate_string($ldap_config_file, $mysql_config_file, $package_ensure)
-  validate_string($package_name, $package_name_ldap, $package_name_mysql)
-  validate_string($package_name_pgsql, $pgsql_config_file, $server_mode)
-  validate_string($server_type, $service_name, $service_provider, $ssl_pemfile)
-  validate_string($uploadgid, $uploaduid, $uploadscript)
 
   if $manage_ssl {
     include '::openssl'
@@ -58,8 +47,8 @@ class pureftpd (
   include '::pureftpd::config'
   include '::pureftpd::service'
 
-  Class[ 'pureftpd::install' ] ->
-  Class[ 'pureftpd::config' ] ->
-  Class[ 'pureftpd::service' ]
+  Class[ 'pureftpd::install' ]
+  -> Class[ 'pureftpd::config' ]
+  -> Class[ 'pureftpd::service' ]
 
 }
